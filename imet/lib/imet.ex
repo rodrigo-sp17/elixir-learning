2c5ca434e0ff6522bf7e29ee949e39cb06244646
Rodrigo Silva <rodrigosilvap93@gmail.com>
2021-03-05 12:53:14 -0300

    Changed Weather program to use GenServer

---
+++ imet/lib/imet.ex
@@ -0,0 +1,18 @@
+defmodule Imet do
+  @moduledoc """
+  Documentation for `Imet`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Imet.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ imet/test/imet_test.exs
@@ -0,0 +1,8 @@
+defmodule ImetTest do
+  use ExUnit.Case
+  doctest Imet
+
+  test "greets the world" do
+    assert Imet.hello() == :world
+  end
+end
